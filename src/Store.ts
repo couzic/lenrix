2ea0c6fb8f96b495e53f3910263b307367507e15
Mikael Couzic <mikaelcouzic@gmail.com>
2017-11-06 14:40:09 +0100

    Store.focusPath() to accept path array

--- src/Store.ts
+++ src/Store.ts
@@ -26,12 +26,22 @@ export interface Store<State> extends ReadableStore<State>, UpdatableStore<State
    focusPath<K extends keyof State>(this: Store<State & NotAnArray>,
                                     key: K): Store<State[K]>
 
+   focusPath<K extends keyof State>(this: Store<State & NotAnArray>,
+                                    path: [K]): Store<State[K]>
+
    focusPath<K1 extends keyof State,
       K2 extends keyof State[K1]>(key1: K1, key2: K2): Store<State[K1][K2]>
 
    focusPath<K1 extends keyof State,
+      K2 extends keyof State[K1]>(path: [K1, K2]): Store<State[K1][K2]>
+
+   focusPath<K1 extends keyof State,
       K2 extends keyof State[K1],
       K3 extends keyof State[K1][K2]>(key1: K1, key2: K2, key3: K3): Store<State[K1][K2][K3]>
+
+   focusPath<K1 extends keyof State,
+      K2 extends keyof State[K1],
+      K3 extends keyof State[K1][K2]>(path: [K1, K2, K3]): Store<State[K1][K2][K3]>
 
    focusPath<K1 extends keyof State,
       K2 extends keyof State[K1],
@@ -41,8 +51,19 @@ export interface Store<State> extends ReadableStore<State>, UpdatableStore<State
    focusPath<K1 extends keyof State,
       K2 extends keyof State[K1],
       K3 extends keyof State[K1][K2],
+      K4 extends keyof State[K1][K2][K3]>(path: [K1, K2, K3, K4]): Store<State[K1][K2][K3][K4]>
+
+   focusPath<K1 extends keyof State,
+      K2 extends keyof State[K1],
+      K3 extends keyof State[K1][K2],
       K4 extends keyof State[K1][K2][K3],
       K5 extends keyof State[K1][K2][K3][K4]>(key1: K1, key2: K2, key3: K3, key4: K4, key5: K5): Store<State[K1][K2][K3][K4][K5]>
+
+   focusPath<K1 extends keyof State,
+      K2 extends keyof State[K1],
+      K3 extends keyof State[K1][K2],
+      K4 extends keyof State[K1][K2][K3],
+      K5 extends keyof State[K1][K2][K3][K4]>(path: [K1, K2, K3, K4, K5]): Store<State[K1][K2][K3][K4][K5]>
 
    focusPath<K1 extends keyof State,
       K2 extends keyof State[K1],
@@ -56,8 +77,23 @@ export interface Store<State> extends ReadableStore<State>, UpdatableStore<State
       K3 extends keyof State[K1][K2],
       K4 extends keyof State[K1][K2][K3],
       K5 extends keyof State[K1][K2][K3][K4],
+      K6 extends keyof State[K1][K2][K3][K4][K5]>(path: [K1, K2, K3, K4, K5, K6]): Store<State[K1][K2][K3][K4][K5][K6]>
+
+   focusPath<K1 extends keyof State,
+      K2 extends keyof State[K1],
+      K3 extends keyof State[K1][K2],
+      K4 extends keyof State[K1][K2][K3],
+      K5 extends keyof State[K1][K2][K3][K4],
       K6 extends keyof State[K1][K2][K3][K4][K5],
       K7 extends keyof State[K1][K2][K3][K4][K5][K6]>(key1: K1, key2: K2, key3: K3, key4: K4, key5: K5, key6: K6, key7: K7): Store<State[K1][K2][K3][K4][K5][K6][K7]>
+
+   focusPath<K1 extends keyof State,
+      K2 extends keyof State[K1],
+      K3 extends keyof State[K1][K2],
+      K4 extends keyof State[K1][K2][K3],
+      K5 extends keyof State[K1][K2][K3][K4],
+      K6 extends keyof State[K1][K2][K3][K4][K5],
+      K7 extends keyof State[K1][K2][K3][K4][K5][K6]>(path: [K1, K2, K3, K4, K5, K6, K7]): Store<State[K1][K2][K3][K4][K5][K6][K7]>
 
    focusFields<K extends keyof State>(this: Store<State & NotAnArray>, ...keys: K[]): Store<Pick<State, K>>
 
