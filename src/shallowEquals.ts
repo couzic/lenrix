e9a2ba54052e8967e4b57e4529a69d65098eaaf1
Mikael Couzic <mikaelcouzic@gmail.com>
2017-11-09 09:35:20 +0100

    shallowEquals() first checks for strict equality

--- src/shallowEquals.ts
+++ src/shallowEquals.ts
@@ -1,4 +1,5 @@
 export function shallowEquals(a: any, b: any) {
+   if (a === b) return true
    for (let i in a) if (!(i in b)) return false
    for (let i in b) if (a[i] !== b[i]) return false
    return true
