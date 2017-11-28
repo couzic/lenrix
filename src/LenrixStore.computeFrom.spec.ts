aad531d7d2681dea2c216930a814e997d377b10b
Mikael Couzic <mikaelcouzic@gmail.com>
2017-11-28 11:21:29 +0100

    Make computeFrom() tests pass

--- src/LenrixStore.computeFrom.spec.ts
+++ src/LenrixStore.computeFrom.spec.ts
@@ -1,7 +1,6 @@
 import { expect } from 'chai'
 import { createLens } from 'immutable-lens'
 
-import { ComputedStore } from './'
 import { createStore } from './createStore'
 import { Store } from './Store'
 
@@ -26,22 +25,34 @@ const initialState: State = {
 describe('LenrixStore.computeFrom()', () => {
 
    const lens = createLens<State>()
-   let store: Store<{ state: State }>
-   let computedStore: ComputedStore<{ normalizedState: State, computedValues: { todoListLength: number } }>
+   let store: Store<{
+      state: State
+      computedValues: {}
+      actions: { toggleFlag: void }
+      dependencies: {}
+   }>
+   let computedStore: Store<{
+      state: State
+      computedValues: { todoListLength: number }
+      actions: { toggleFlag: void }
+      dependencies: {}
+   }>
    let state: State
    let stateTransitions: number
 
    beforeEach(() => {
       store = createStore(initialState)
-      computedStore = store.computeFrom({
-         todoList: store.lens.focusPath('todo', 'list')
-      }, selection => ({
+         .actionTypes<{ toggleFlag: void }>()
+         .actionHandlers(_ => ({ toggleFlag: () => _.focusPath('flag').update(flag => !flag) }))
+      computedStore = store.computeFrom(_ => ({
+         todoList: _.focusPath('todo', 'list')
+      }), selection => ({
          todoListLength: selection.todoList.length
       }))
    })
 
    it('computes values', () => {
-      expect(computedStore.currentState.todoListLength).to.equal(initialState.todo.list.length)
+      expect(computedStore.currentComputedState.todoListLength).to.equal(initialState.todo.list.length)
    })
 
    it('does not compute values when unselected fields change', () => {
@@ -49,9 +60,7 @@ describe('LenrixStore.computeFrom()', () => {
       computedStore.state$.subscribe(() => ++stateTransitions)
       expect(stateTransitions).to.equal(1)
 
-      computedStore.updateFields({
-         flag: value => !value
-      })
+      computedStore.actions.toggleFlag(undefined)
 
       expect(stateTransitions).to.equal(1)
    })
