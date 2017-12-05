575cc2cd04b46d2aac6de40585ebb252170bbdfb
Mikael Couzic <mikaelcouzic@gmail.com>
2017-12-05 10:43:58 +0100

    Console logger for messages, updates and epics

--- src/logger/createLogger.ts
+++ src/logger/createLogger.ts
@@ -8,10 +8,16 @@ const doNothing = (...params: any[]) => { }
 
 const createMessageLogger = (reduxStore: Store<any>, options: LoggerOptions): Logger['message'] => {
    const logToConsole = options.console!.message
-      ? (action: FocusedAction) => console.log('[MESSAGE]' + action.type)
+      ? (action: FocusedAction) => {
+         console.groupCollapsed('%c 🔎 MESSAGE', 'background-color: rgb(128, 128, 0); color: #fff; padding: 2px 8px 2px 0; border-radius:6px;', action.type)
+         console.log('payload', action.payload)
+         console.log('meta', action.meta)
+         console.groupEnd()
+         console.log('[MESSAGE]' + action.type)
+      }
       : doNothing
    const logToRedux = options.redux!.message
-      ? (action: FocusedAction) => reduxStore.dispatch({ type: '[MESSAGE]' + action.type })
+      ? (action: FocusedAction) => reduxStore.dispatch({ type: '[MESSAGE]' + action.type, payload: action.payload, meta: action.meta })
       : doNothing
    return (action: FocusedAction) => {
       logToConsole(action)
@@ -21,16 +27,26 @@ const createMessageLogger = (reduxStore: Store<any>, options: LoggerOptions): Lo
 
 const createUpdateLogger = (options: LoggerOptions): Logger['update'] => {
    return options.console!.update
-      ? (action: FocusedAction) => console.log('[UPDATE]' + action.type)
+      ? (action: FocusedAction) => {
+         console.groupCollapsed('%c 🔎 UPDATE', 'background-color: rgb(32, 128, 32); color: #fff; padding: 2px 8px 2px 0; border-radius:6px;', action.type)
+         console.log('payload', action.payload)
+         console.log('meta', action.meta)
+         console.groupEnd()
+      }
       : doNothing
 }
 
 const createEpicLogger = (reduxStore: Store<any>, options: LoggerOptions): Logger['epic'] => {
    const logToConsole = options.console!.epic
-      ? (action: FocusedAction) => console.log('[EPIC]' + action.type)
+      ? (action: FocusedAction) => {
+         console.groupCollapsed('%c 🔎 EPIC', 'background-color: rgb(128, 32, 32); color: #fff; padding: 2px 8px 2px 0; border-radius:6px;', action.type)
+         console.log('payload', action.payload)
+         console.log('meta', action.meta)
+         console.groupEnd()
+      }
       : doNothing
    const logToRedux = options.redux!.epic
-      ? (action: FocusedAction) => reduxStore.dispatch({ type: '[EPIC]' + action.type })
+      ? (action: FocusedAction) => reduxStore.dispatch({ type: '[EPIC]' + action.type, payload: action.payload, meta: action.meta })
       : doNothing
    return (action: FocusedAction) => {
       logToConsole(action)
@@ -38,27 +54,12 @@ const createEpicLogger = (reduxStore: Store<any>, options: LoggerOptions): Logge
    }
 }
 
-// const shouldLogAsArray = (previous: any, next: any): boolean =>
-//    (Array.isArray(previous) && (Array.isArray(next) || typeof next !== 'object'))
-//    || (Array.isArray(next) && (Array.isArray(previous) || typeof previous !== 'object'))
-
-// const shouldLogAsObject = (previous: any, next: any): boolean =>
-//    (!Array.isArray(previous) && !Array.isArray(next))
-//    && (typeof previous === 'object' || typeof next === 'object')
-
-// const logAny = (previous: any, next: any) => {
-//    if (shouldLogAsArray(previous, next)) {
-//    } else if (shouldLogAsObject(previous, next)) {
-//    } else {
-//    }
-// }
-
 const createComputeLogger = (reduxStore: Store<any>, options: LoggerOptions): Logger['compute'] => {
    const loggableKeys = (previous: object, next: object): string[] => Object.keys(Object.assign(previous, next))
    const logToConsole = options.console!.compute
       ? (previous: any, next: any) => {
          const keys = Object.keys(Object.assign(previous, next))
-         console.groupCollapsed('%c 🔎 COMPUTE', 'background-color: rgb(32, 32, 128); color: #fff; padding: 2px 8px 2px 0; border-radius:5px;', keys)
+         console.groupCollapsed('%c 🔎 COMPUTE', 'background-color: rgb(32, 32, 128); color: #fff; padding: 2px 8px 2px 0; border-radius:6px;', keys)
          keys.forEach(key => console.log(key, ':', previous[key], '→', next[key]))
          console.groupEnd()
       }
