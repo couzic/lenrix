94f1daae8f3c9e6c0b506a5bf18b07f7cac42e49
Mikael Couzic <mikaelcouzic@gmail.com>
2018-06-19 16:39:20 +0200

    0.4.8 - Store.filter()

--- src/index.ts
+++ src/index.ts
@@ -10,6 +10,3 @@ export * from './Store'
 // logger
 export * from './logger/LoggerOptions'
 export * from './logger/silentLoggerOptions'
-
-// test-utils
-export * from './test-utils/cloneIsolated'
